e54166b442a4aa17e06c65df9e4ecfbc38fbf6de
Helio Leao <hmtleao@hotmail.com>
2024-12-02 10:23:50 -0300

    feat: swagger ui express setup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import authRouter from "./routes/auth";
 import productRouter from "./routes/products";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/auth", authRouter);
 app.use("/products", productRouter);
+
+app.get("/api-docs.json", (_req, res) => {
+  res.json(swaggerDocument);
+});
 
 app.all("*", (_req, res) => {
   res.sendStatus(404);
